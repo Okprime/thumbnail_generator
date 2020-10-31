651dd7d5a06ee2676d04e1698298683844371210
Uwede <ouwede@terragonltd.com>
2020-10-31 15:12:47 +0100

    code update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,110 @@
+import React, { useRef, useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const imageRef = useRef();
+  const [data, setData] = useState([]);
+  const [uploading, setUploading] = useState(false);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/upload`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then(({ data, message }) => {
+        setData(data);
+      });
+  }, []);
+
+  const uploadImage = (e) => {
+    e.preventDefault();
+
+    setUploading(true);
+
+    const formData = new FormData();
+    formData.append('image', imageRef.current.files[0]);
+
+    fetch(`${process.env.REACT_APP_API_URL}/upload`, {
+      method: 'POST',
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then(({ data, message }) => {
+        console.log('Data:: ', data);
+
+        if (data && data.original) {
+          setData((d) => [data, ...d]);
+        }
+      })
+      .finally(() => {
+        setUploading(false);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <form
+          onSubmit={(e) => uploadImage(e)}
+          style={{
+            marginBottom: 20,
+          }}
         >
-          Learn React
-        </a>
+          <input
+            type="file"
+            name="image"
+            accept=".png, .jpg, .jpeg"
+            ref={imageRef}
+          />
+          <button type="submit">Upload</button>
+          {uploading && (
+            <div>
+              <span style={{ fontSize: '10px' }}>Uploading file . . .</span>
+            </div>
+          )}
+        </form>
+
+        <div
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between',
+          }}
+        >
+          {data.map(({ original, thumbnail }) => (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                margin: 10,
+                width: 300,
+              }}
+            >
+              <img
+                src={original}
+                alt="original"
+                style={{
+                  maxWidth: 300,
+                  maxHeight: 200,
+                }}
+              />
+              <img
+                src={thumbnail}
+                alt="thumbnail"
+                style={{
+                  maxWidth: 60,
+                  maxHeight: 40,
+                  marginTop: 10,
+                }}
+              />
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
